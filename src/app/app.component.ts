2789beaa270b531e11ff7c1b7cef5f4c4863126f
Phillipcodes <Phillipsauer00@gmail.com>
2024-08-31 11:55:02 +0200

    Start of Portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser'
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent,AboveTheFoldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfoliopms';
+
+  constructor(private titleService: Title) {
+ this.titleService.setTitle('Phillip-Marcel Sauer')
+  }
 }
