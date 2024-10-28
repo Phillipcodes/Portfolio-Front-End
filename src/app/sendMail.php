64f7b26b794372d6fa50df039f25ed60a77225a8
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-28 11:11:01 +0100

    updated php script

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'phillip@phillip-marcel-sauer.com';  
+            $recipient = 'contact@phillip-marcel-sauer.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
